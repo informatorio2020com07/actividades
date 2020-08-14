18233f862326e347393be115296e31262775dfd1
lugon <lugon399@gmail.com>
2020-08-14 20:11:43 -0300

    004: permite al usuario escribir modelos

--- gonzalez_luciano/004/004.py
+++ gonzalez_luciano/004/004.py
@@ -1,42 +1,83 @@
 modelos = [
+# modelos de prueba
+
 # -- 0
-"""Estimado {}:
+"""Estimado <des>:
     Le damos la bienvenida al servicio.
 
 Con afecto,
-    {}""",
+    <rem>
+""",
 
 # -- 1
-"""Hola {}, bienvenido!
+"""Hola <des>, bienvenido!
 
-Atte, {}"""
+Atte, <rem>
+"""
 ]
 
-def generar_notas(modelo, destinatarios, remitente):
-    for (i, d) in enumerate(destinatarios):
-        if i > 0:
-            print("---")
-        print(modelo.format(d, remitente))
-
-for (i, m) in enumerate(modelos):
-    print("###### Modelo", i)
-    generar_notas(m, ["<nombre>"], "<remitente>")
+def mostrar_modelos():
     print()
+    for i, m in enumerate(modelos):
+        if i > 0:
+            print("-"*10 + "\n")
+        print("#### Modelo ", i, ":", sep="")
+        print(m)
+
+def leer_modelo():
+    print("-"*10)
+    print("Escriba su modelo.")
+    print("Puede utilizar varias lineas.")
+    print("Ingrese una linea en blanco para terminar.")
+    print("Utilize <des> donde quiere que aparezca el nombre del destinatario.")
+    print("Y similarmente <rem> para el remitente.")
+
+    lineas = []
+    while True:
+        linea = input("> ")
+        if len(linea) == 0:
+            break
+        lineas.append(linea + "\n")
+    modelos.append("".join(lineas))
+
+def generar_notas():
+    while True:
+        nro_modelo = int(input("Ingrese un numero de modelo (del 0 al {}): ".format(len(modelos)-1)))
+        if nro_modelo>=0 and nro_modelo<len(modelos):
+            modelo = modelos[nro_modelo]
+            break
+
+    destinatarios = []
+    while True:
+        dest = input("Ingrese un destinatario (vacio para terminar): ")
+        if len(dest) == 0:
+            break
+        destinatarios.append(dest)
+
+    remitente = input("Ingrese un remitente: ")
+
+    print()
+    for i, d in enumerate(destinatarios):
+        if i > 0:
+            print("-"*10 + "\n")
+        print(modelo.replace("<des>", d).replace("<rem>", remitente))
 
 while True:
-    modelo_selec = int(input("Seleccione un modelo: "))
-    if modelo_selec>=0 and modelo_selec<len(modelos):
+    print("1. Ver modelos")
+    print("2. Ingresar modelo")
+    print("3. Generar nota")
+    print("4. Salir")
+
+    while True:
+        opcion = int(input("Ingrese una opción: "))
+        if opcion>=1 and opcion<=4:
+            break
+
+    if opcion == 1:
+        mostrar_modelos()
+    elif opcion == 2:
+        leer_modelo()
+    elif opcion == 3:
+        generar_notas()
+    else:
         break
-
-destinatarios = []
-
-while True:
-    d = input("Ingrese un destinatario (vacio para terminar): ")
-    if len(d) == 0:
-        break
-    destinatarios.append(d)
-
-remitente = input("Ingrese un remitente: ")
-
-print()
-generar_notas(modelos[modelo_selec], destinatarios, remitente)
