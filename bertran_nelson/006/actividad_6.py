2e4e7299476cd9d36cc48140a27016f6b3624b10
NeelBit <nelbertran@gmail.com>
2020-08-20 16:18:56 -0300

    Actividad 006 terminada

--- bertran_nelson/006/actividad_6.py
+++ bertran_nelson/006/actividad_6.py
@@ -5,3 +5,21 @@
 	eliminados.
 """
 
+def eliminaDuplicados(listaNumeros):
+
+	duplicados = []
+
+	for i in listaNumeros:
+		if i in listaNumeros[listaNumeros.index(i)+1:]:
+
+			duplicados.append(i)
+
+	listaNueva = set(listaNumeros)
+
+	return f"La lista sin duplicados es: {list(listaNueva)} y la tupla de duplicados: {tuple(duplicados)}"
+	
+"""
+listaPrueba = [1, 2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 10, 11, 4, 3, 2, 1]
+print(eliminaDuplicados(listaPrueba))
+La lista sin duplicados es: [1, 2, 3, 4, 5, 10, 11] y la tupla de duplicados: (1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 4, 3, 2, 1)
+"""
