49385e7aecf21c703040fa54e63a292fcd46c311
cdvcristiann <cristianndvillordo11@gmail.com>
2020-09-02 11:24:50 -0300

    borre lo que me agrego al aparecer un conflicto en el repositorio

--- casabuena_damian/001/001.py
+++
@@ -1,6 +0,0 @@
-precio= float(input("Introduzca el precio del producto:"))
-
-cant_unidades=int(input("Unidades vendidas:"))
-
-total=print(precio*cant_unidades)
-
--- gutierrez_eliseo/001/001.py
+++
@@ -1,8 +0,0 @@
-
-print("precio de productos")
-
-producto = float(input("El precio del producto: "))
-
-unidades_vend = int(input("Unidades vendidas este mes: "))
-
-print("El total de las ventas del mes es: ", producto * unidades_vend)
