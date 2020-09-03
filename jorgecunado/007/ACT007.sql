47019329cea085228484b1c3617279e8a0333592
jorge cunado <cjrssj9@gmail.com>
2020-09-03 17:40:01 -0300

    cambios sugeridos por el profesor van on ejemplos

--- jorgecunado/007/ACT007.sql
+++ jorgecunado/007/ACT007.sql
@@ -13,9 +13,8 @@ CREATE TABLE personas (
   Id int AUTO_INCREMENT PRIMARY KEY,
   Nombre varchar(20) NOT NULL,
   Apellido varchar(20) NOT NULL,
-  Nacimiento date NOT NULL,
-  Rol int NOT NULL,
-  FOREIGN KEY(Rol) REFERENCES roles(id)
+  Nacimiento date NOT NULL
+   
 );
 
 CREATE TABLE peliculas (
@@ -29,6 +28,9 @@ CREATE TABLE personasenpeliculas (
   Id int AUTO_INCREMENT PRIMARY KEY,
   Id_persona int NOT NULL,
   Id_pelicula int NOT NULL,
+  id_rol int NOT NULL,
+
+  FOREIGN KEY (Id_rol) REFERENCES roles (id),
   FOREIGN KEY (Id_persona) REFERENCES personas (id),
   FOREIGN KEY (Id_pelicula) REFERENCES peliculas (id)
 );
@@ -40,15 +42,13 @@ INSERT INTO roles (Descripcion) VALUES
  ('Director')
 ; 
  
-INSERT INTO personas (Nombre,Apellido,Nacimiento,Rol) VALUES 
- ('Robert','De Niro','1946-02-19',1),
- ('Brad','Pit','1965-12-19',1),
- ('Romeo','Santos','2000-12-19',2),
- ('Elena','Vinder','2011-12-19',2),
- ('Roman','Polansky','1950-12-19',4),
- ('Steven','Spilberg','1955-12-19',1),
- ('Steven','Spilberg','1965-12-19',3),
- ('Roman','Polansky','1950-12-19',1)
+INSERT INTO personas (Nombre,Apellido,Nacimiento) VALUES 
+ ('Robert','De Niro','1946-02-19'),
+ ('Brad','Pit','1965-12-19'),
+ ('Romeo','Santos','2000-12-19'),
+ ('Elena','Vinder','2011-12-19'),
+ ('Steven','Spilberg','1955-12-19'),
+ ('Roman','Polansky','1950-12-19')
 ; 
 
 INSERT INTO peliculas (Nombrep,Estreno,Costo) VALUES 
@@ -57,22 +57,23 @@ INSERT INTO peliculas (Nombrep,Estreno,Costo) VALUES
  ('COSMOS 1999','1980-01-01',500000)
 ; 
 
-INSERT INTO personasenpeliculas (Id_persona,Id_pelicula) VALUES
-(1,1),
-(4,1),
-(5,1),
-(2,2),
-(3,2),
-(6,2),
-(7,2),
-(1,3),
-(6,3)
+INSERT INTO personasenpeliculas (Id_persona,Id_pelicula,Id_rol) VALUES
+(1,1,1),
+(1,1,3),
+(4,1,2),
+(5,1,4),
+(2,2,1),
+(3,2,2),
+(6,2,4),
+(5,2,3),
+(1,3,1),
+(6,3,1)
 ;
 
 SELECT p.nombrep, pe.nombre, pe.apellido, r.descripcion 
-from peliculas p, personas pe, roles r, personasenpeliculas pp  
-where pe.rol = r.id AND pp.id_persona = pe.id AND pp.id_pelicula = p.id;
+FROM peliculas p, personas pe, roles r, personasenpeliculas pp  
+WHERE pp.id_rol = r.id AND pp.id_persona = pe.id AND pp.id_pelicula = p.id;
 
 SELECT p.nombrep pelicula, pe.nombre , pe.apellido apellido_actor, r.descripcion participacion
-from personasenpeliculas pp JOIN peliculas p JOIN personas pe JOIN roles r 
-on pp.id_pelicula = p.id AND pp.id_persona = pe.id AND pe.rol = r.id;
+FROM personasenpeliculas pp JOIN peliculas p JOIN personas pe JOIN roles r 
+on pp.id_pelicula = p.id AND pp.id_persona = pe.id AND pp.id_rol = r.id;
