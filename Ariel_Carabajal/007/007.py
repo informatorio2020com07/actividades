22bfcbaa207f725e25d8244c60815297a78c3554
Ariel <arieltuz@gmail.com>
2020-09-06 17:50:48 -0300

    agrego los ejercicios

---
+++ Ariel_Carabajal/007/007.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+007-
+    Realizar modelo entidad relación, luego pasar a tablas.
+    Realizar un script para mysql que cree las correspondientes tablas.
+    Realizar un script que inserte datos en las distintas tablas.
+
+    Caso:
+        Un actor puede actuar en 1 o más películas.
+        En una película pueden actuar varios actores.
+        A una película la puede dirigir uno o varios directores, y cada director 
+        pudo haber dirigido varias películas.
+
+        De los actores, se quiere conocer: nombre, apellido y fecha de nacimiento.
+        De los directores, se quiere conocer: nombre, apellido y fecha de nacimiento.
+        De las películas, se quiere conocer: nombre, fecha de estreno y costo total.
+'''
