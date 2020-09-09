7baf03eb8fe1092d3f3acdf85972b6e0d6b3ff11
Ariel <arieltuz@gmail.com>
2020-09-09 01:05:46 -0300

    archivos subidos

--- Villordo_Cristian/001/001.py
+++
@@ -1,9 +0,0 @@
-# 001-
-#   Programa que pregunte al usuario, el precio del producto y la cantidad de unidades vendidas.
-#   Informar el total de ventas.
-
-print("PRECIO DE PRODUCTOS")
-producto=float(input())
-print("CANTIDAD DE UNDIDADES VENDIDAS EN EL MES")
-unidades=float(input())
-print("el total de las ventas del mes es ", producto*unidades)
--- meza_cristian/001/act_1.py
+++
@@ -1,8 +0,0 @@
-'''
-Programa que pregunte al usuario, el precio del producto y la cantidad de unidades vendidas.
-Informar el total de ventas.
-'''
-print("Calculador de Ventas: ")
-precio=float(input("Ingresar Precio del Producto $: "))
-cantidad=int(input("Ingresar cantidad de unidades Vendidas : "))
-print("El Importe Total de las Ventas Realizadas es: ${}".format(precio*cantidad))
